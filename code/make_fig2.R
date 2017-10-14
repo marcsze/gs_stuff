e0f476277b64de26e3e0ace7ab350ded8e581f5b
marcsze <marcasze@gmail.com>
2017-10-14 16:31:35 -0400

    first commit

---
+++ code/make_fig2.R
@@ -0,0 +1,11 @@
+### Add the second graph to be used in presentation
+### What are the most important OTUs to this model (Look at Top 30)?
+### Marc Sze
+
+
+# Load needed libraries
+library(tidyverse)
+library(viridis)
+
+# Load needed data
+graph_data <- read_csv("data/process/tables/imp_otus_to_model.csv")
