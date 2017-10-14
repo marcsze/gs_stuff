20622335a49ca7271433beec6f5b616e193577c5
marcsze <marcasze@gmail.com>
2017-10-14 16:47:04 -0400

    complete fig 2 code

--- code/make_fig2.R
+++ code/make_fig2.R
@@ -8,4 +8,24 @@ library(tidyverse)
 library(viridis)
 
 # Load needed data
-graph_data <- read_csv("data/process/tables/imp_otus_to_model.csv")
+graph_data <- read_csv("data/process/tables/imp_otus_to_model.csv") %>% 
+  slice(1:30) %>% arrange(mda)
+
+
+# Create the graph of the data
+imp_otu_graph <- graph_data %>% 
+  mutate(otu = factor(otu, levels = graph_data$otu)) %>% 
+  ggplot(aes(x = otu, y = mda)) + 
+  geom_point(show.legend = F, size = 4) + 
+  coord_flip() + 
+  theme_bw() + 
+  labs(y = "Mean Decrease in Accuracy", x = "") + 
+  theme(axis.title.y = element_text(face = "bold"), 
+        panel.grid.minor = element_blank(),
+        panel.grid.major.x = element_blank(), 
+        axis.text = element_text(size = 13))
+
+# Write out the figure
+ggsave("results/figures/imp_otu_figure.tiff", imp_otu_graph, width = 6, height = 5, dpi = 300)
+
+
