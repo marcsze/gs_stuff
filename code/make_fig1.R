e2ebbe65d780470b357c0abdea4c1c74af9efffb
marcsze <marcasze@gmail.com>
2017-10-14 15:55:16 -0400

    first commit

---
+++ code/make_fig1.R
@@ -0,0 +1,16 @@
+### Add the first graph to be used in presentation
+### Do the probabilities of the community to be an adult (mother) increase over time?
+### Marc Sze
+
+
+# Load needed libraries
+library(tidyverse)
+
+# Load needed data
+graph_data <- read_csv("data/process/tables/probability_scores.csv")
+
+
+
+
+
+
