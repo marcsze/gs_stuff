bc5c1f089f82822a52ebf50b934b527e588289b7
marcsze <marcasze@gmail.com>
2017-10-15 12:10:17 -0400

    complete read out code

--- code/run_get_imp_otu_list.R
+++ code/run_get_imp_otu_list.R
@@ -14,6 +14,7 @@ imp_otu_data <- as.data.frame(read_csv("data/process/tables/imp_otus_to_model.cs
 
 # Write out only the top 30 OTUs
 
-write(imp_otu_data[, "otu"], "data/process/select_otus", ncolumns = length(imp_otu_data[, "otu"]), sep = " ")
-write.table(imp_otu_data, "data/process/select_otus", quote = F, col.names = F, row.names = F)
 
+fileConn<-file("data/process/select_otus")
+writeLines(imp_otu_data[, "otu"], fileConn, sep = " ")
+close(fileConn)
