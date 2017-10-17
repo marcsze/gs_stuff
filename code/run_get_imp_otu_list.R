8140c003f069dde29fc0bdddeff0eeabc2653b82
marcsze <marcasze@gmail.com>
2017-10-16 20:35:38 -0400

    add comments to code

--- code/run_get_imp_otu_list.R
+++ code/run_get_imp_otu_list.R
@@ -12,9 +12,7 @@ imp_otu_data <- as.data.frame(read_csv("data/process/tables/imp_otus_to_model.cs
 
 
 
-# Write out only the top 30 OTUs
-
-
+# Write out only the top 30 OTUs, these will be written out as a string
 fileConn<-file("data/process/select_otus")
 writeLines(imp_otu_data[, "otu"], fileConn, sep = " ")
 close(fileConn)
--- code/run_wilcoxson_test.R
+++ code/run_wilcoxson_test.R
@@ -17,12 +17,17 @@ test_data <- read_csv("data/process/tables/probability_scores.csv")
 
 # Function to run the wilcox test and return the p-value
 run_test <- function(first_group, second_group, dataTable = test_data){
+  # first_group stands for the first comparison group
+  # second_group stands for the second comparison group
+  # dataTable is defaulted to test_data to allow for easier use with mapply
   
+  # Generate the first comparison group
   firstData <- dataTable %>% filter(TIME_OF_SAMPLING == first_group)
+  # Generate the second comparison group
   secondData <- dataTable %>% filter(TIME_OF_SAMPLING == second_group)
-  
+  # Generate the pvalue from comparing the two groups
   tempResult <- wilcox.test(firstData$yes_adult, secondData$yes_adult)$p.value
-  
+  # write out the pvalue to the global work environment
   return(tempResult)
 }
 
@@ -32,10 +37,11 @@ run_test <- function(first_group, second_group, dataTable = test_data){
 ###################################### Execute the given functions #############################################
 ################################################################################################################
 
-#Set up tests
+#First group to cycle through
 first_var <- c("3rd_tri", "3rd_tri", "3rd_tri", "1_month_post", "1_month_post", "6_month_old")
+# Second group to cycle through
 second_var <- c("1_month_post", "6_month_old", "4_years_old", "6_month_old", "4_years_old", "4_years_old")
-
+# generate the final data table
 final_table <- data_frame(
   g1 = first_var, g2 = second_var, pvalue = mapply(run_test, first_var, second_var)) %>% 
   mutate(bh = p.adjust(pvalue, method = "BH"))
